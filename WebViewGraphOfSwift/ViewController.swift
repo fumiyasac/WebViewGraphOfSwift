37d002c99c71760683ddc03dde5425b83b716078
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2015-12-17 01:59:15 +0900

    initial commit & create layouts & install Realm

---
+++ WebViewGraphOfSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewGraphOfSwift
+//
+//  Created by 酒井文也 on 2015/12/15.
+//  Copyright © 2015年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
